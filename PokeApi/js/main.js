6e2b895c8072544992c08ab219e9de49dddfdfbc
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-09-08 22:27:36 -0300

    mejora de pag, imgs agregadas 08/09/2022

--- PokeApi/js/main.js
+++ PokeApi/js/main.js
@@ -1,10 +1,14 @@
+// hago conexiones con html para usar el DOM
 const listaPokes = document.getElementById("listaDePokes");
-const loading = document.getElementById("loading")
-const obtenerPokes = async (poke = "") =>{
+const loading = document.getElementById("loading");
+
+// Sirve para caapitalizar una palabra (Vuelve la mayúscula la primera letra)
+const capitalizar = (palabra)=>palabra.replace(/^\w/,(c) => c.toUpperCase());
+
+
+//Sirve para obtener todos los pokemones de la api (creo)
+const obtenerPokes = async () =>{
     try {
-        (poke.trim())
-        ? ""
-        : "";
         let consulta = await fetch(`https://pokeapi.co/api/v2/pokedex/national/`)
         const res = await consulta.json()
         return res.pokemon_entries
@@ -13,53 +17,104 @@ const obtenerPokes = async (poke = "") =>{
         return error
     }
 }
+// obtengo la información de cada pokémon, puedo ingresar como primer parámetro el ID o nombre pokémon
+// el segundo parámetro solo acepta "img" para devolver la url de una imagen
+// o types para devolver el nombre del tipo elemental del pokémon
 const obtenerPokeInfo = async (pokeID,imgOData) => {
     try {
-        return await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)
-        .then(res => res.json())
-        .then(Poke=>{if(imgOData === "img"){
+        const consulta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeID}/`)
+        const Poke = await consulta.json();
+        if(imgOData === "img"){
            return Poke.sprites.front_default
         }else if(imgOData === "types"){
-            return [Poke.types[0].type.name , Poke.types[1]?.type.name]
-        }})
+            return [Poke.types[0]?.type.name , Poke.types[1]?.type?.name]
+        }
         
     } catch (error) {
         return error
     }
 }
-const pintarPokes = async () => {
+
+// inicializo la cantidad de pokemones y el límite de renderizado iniciado con 30
+
+let pokesCant = 0;
+let limitePokes = 30;
+// con este for renderizo tarjetas simulando que carga la página mientras carga la información
+// donde solo renderizo 12 tarjetas
+    for(let i=0;i<12;i++){
+        listaPokes.innerHTML += `
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2 mt-5">
+                        
+                        
+        <div class="card" aria-hidden="true">
+                <img src="..." class="card-img-top "  alt="">
+                <div class="card-body">
+                    <h5 class="card-title placeholder-glow">
+                    <span class="placeholder col-6">1111111111111111111111</span>
+                    </h5>
+                    <p class="card-text placeholder-glow">
+                    <span class="placeholder col-7">111111111111111</span>
+                    <span class="placeholder col-4">11111111111</span>
+                    <span class="placeholder col-4">1111111111</span>
+                    <span class="placeholder col-6">11111111111111</span>
+                    <span class="placeholder col-8">111111111111</span>
+                    </p>
+                    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+                </div>
+            </div>
+        </div>`
+    }
+    
+// con esta fnAsync pinto las tarjetas de los pokemones
+// el parámetro que recibe por defecto es false y quiere decir que si se usa la fn
+// es como si se reiniciara la página, en caso de true recarga la página pero con más pokemones(nuevo límite +30)
+const pintarPokes = async (boolean=false) => {
     try {
         let resultado = await obtenerPokes();
-        let rendeHTML = "";
-         resultado.forEach( async (element, index) => {
-            let pokeIMGS = await obtenerPokeInfo(index+1,"img");
-            let pokeInfoType = await obtenerPokeInfo(index+1, "types");
-            let nombre = await element.pokemon_species.name;
-                rendeHTML += `
+
+        listaPokes.innerHTML="";
+        if(boolean){pokesCant=0;limitePokes+=30}else{pokesCant=0;limitePokes=30}
+        // este for usa el liminte de pokemones y se maneja por el valor de pokesCant actual
+         for(pokesCant;pokesCant<limitePokes;pokesCant++){
+            //obtengo las imagenes por cada pokémon
+            let pokeIMGS = await obtenerPokeInfo(pokesCant+1,"img");
+            // obtengo la información de tipo elemental de cada pokémon
+            let pokeInfoType = await obtenerPokeInfo(pokesCant+1, "types");
+            // obtengo el nombre de cada pokémon
+            let nombre = await resultado[pokesCant].pokemon_species.name;
+            // cargo la tarjeta en listaPokes que es un .row y se va a ir sumando una tarjeta por pokémon
+            // hasta un tome minimo de 30
+                listaPokes.innerHTML += `
             
-                <div class="col-6 col-md-6 col-lg-4 mt-5" id="Poke${index}">
-                    <div class="card p-3" style="width: 18rem;">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2 mt-5" id="Poke${pokesCant}">
+                    <div class="card p-3 w-auto" >
                         <img src="${pokeIMGS}" class="card-img-top img-fluid" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">${nombre.replace(/^\w/,(c) => c.toUpperCase())}</h5>
-                            <p class="card-text">${pokeInfoType[0]} ${pokeInfoType[1]}</p>
+                            <h5 class="card-title">${capitalizar(nombre)}  </h5>
+                            <img src="./img/240px-Pokémon_${capitalizar(pokeInfoType[0])}_Type_Icon.svg.png" class="Poketype" alt="...">
+
+                            ${typeof pokeInfoType[1]== "undefined"?"":`
+                                <img src="./img/240px-Pokémon_${capitalizar(pokeInfoType[1])}_Type_Icon.svg.png" class="Poketype" alt="...">
+                            `}  
+
+                            <p class="card-text">
+                                ${capitalizar(pokeInfoType[0])} ${typeof pokeInfoType[1]== "undefined"?"":capitalizar(pokeInfoType[1])}
+                            </p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>`
-            
-            
-         });
+         }
+         //limpio el innerHTML de la animación de "cargando..."
          setTimeout(()=>{
             loading.innerHTML = ``;
-            listaPokes.innerHTML="";
-            listaPokes.innerHTML= rendeHTML
-         },12000)
+            
+            
+         },3000)
 
          
     } catch (error) {
         console.log(error)
     }
 }
-
-
+pintarPokes()
